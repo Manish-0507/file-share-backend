7c64fd4023ad13f3eb89f8898eca0890d482e250
Mukesh <manishsharma1582000@gmail.com>
2021-03-01 04:10:21 +0530

    nodemailer error on heroku

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require('path');
- const cors = require('cors');
 
 const connectDB = require('./config/db');
 connectDB();
 
-cors
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(','),
-    //['http://localhost:3000','http://localhost:3000','http://localhost:3000',]//separated by comma like that.
-}
+
 app.use(cors(corsOptions));
 app.use(express.static('public'));
 app.use(express.json());//if any json data is encountered then it will able to parse it
