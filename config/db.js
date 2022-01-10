f8ab8bed1799484e1e5029bcd2c87f3def748ae9
Mukesh <manishsharma1582000@gmail.com>
2022-01-10 20:10:47 +0530

    database reinitiated

--- config/db.js
+++ config/db.js
@@ -1,21 +1,20 @@
-  
 require('dotenv').config();
 const mongoose = require('mongoose');
 function connectDB() {
-    // Database connection 🥳
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-        console.log('Database connected 🥳🥳🥳🥳');
-    });
-    connection.on('error', () => {
-        console.log('error connecting db')
-    }
-        )
-    
-
+	// Database connection
+	mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: true
+	});
+	const connection = mongoose.connection;
+	connection.once('open', () => {
+		console.log('Database connected');
+	});
+	connection.on('error', () => {
+		console.log('error connecting db');
+	});
 }
-
-
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 const connectDB = require('./config/db');
 connectDB();
 
 app.use(express.static('public'));
-app.use(express.json());//if any json data is encountered then it will able to parse it
+app.use(express.json()); //if any json data is encountered then it will able to parse it
 
 //template engine
 app.set('views', path.join(__dirname, '/views'));
@@ -16,113 +16,14 @@ app.set('view engine', 'ejs');
 
 //Add Access Control Allow Origin Headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
-// Routes 
+// Routes
 app.use('/api/files', require('./Routes/Files'));
 app.use('/files', require('./Routes/show'));
 app.use('/files/download', require('./Routes/download'));
 
-
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
